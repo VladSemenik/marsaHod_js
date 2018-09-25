60e63be71a60d260f915830dc982d761d5fe3a61
VladSemenik <vlad.semenik@gmail.com>
2018-09-26 01:08:00 +0300

    new_vrsion_min_operation

--- src/main.java
+++ src/main.java
@@ -10,20 +10,49 @@ public class main {
         
         main L = new main();
         int col = 0;
+        int p;
         
-        L.a(1, 0, position, col);
+    	if(position == 0 || position == 1){
+    		if(position == 0)
+    			System.out.println("\r\nномер итерации = "+col);
+    		else{
+    			System.out.print("A");
+    			System.out.println("\r\nномер итерации = "+col);
+    		}
+    	}else{
+    		if(position < 0){
+    			position *=-1;
+    			System.out.print("R");
+    		}
+	        for(int v = 1; v < Math.abs(position); v=1+v*2){
+				if(Math.abs(position) >= v && Math.abs(position) <= 1+v*2){
+					p = Math.abs(position)-v;
+					if(Math.abs(position) == v)
+						L.a(1, 0, position, position, col);
+					else{
+						if(Math.abs(position)-v > 1+v*2-Math.abs(position)){
+							L.a(1, 0, position, position, col);
+						}else
+							 L.a(1, 0, p, position, col);
+						}
+					}
+			}
+    	}
+       
         
 	}
-	public void a(int v, int curP, int position, int col){
+	public void a(int v, int curP, int position, int imaginary, int col){
 		
-		if((curP > position && v > 0) || (curP < position && v < 0) || Math.ceil(Math.abs(v/2)) >= Math.abs(position-curP)){
-		
-			if(v>=0){
-				v=-1;
-			}else v=1;
+		if(((curP > position && v > 0) || (curP < position && v < 0) || Math.ceil(Math.abs(v/2)) >= Math.abs(position-curP)) && (Math.abs(position-curP) < Math.abs(position-(curP + v)))){
+			int k = v;
+				if(v>=0){
+					v=-1;
+				}else {
+					v=1;
+				}
 			col++;
 			System.out.print('R');
-			a(v, curP, position, col);
+			a(v, curP, position, imaginary, col);
 			
 		}else{
         	curP += v;
@@ -31,13 +60,17 @@ public class main {
     		col++;
     		System.out.print("A");
     		if(position == curP){
-    			
+    			if(position == curP &&  curP == imaginary)
     			System.out.println("\r\nномер итерации = "+col);
-    		
+    			else{
+	    			position = imaginary;
+	    			a(v, curP, position, imaginary, col);
+	    		}
     		}else
-    		a(v, curP, position, col);
+    		a(v, curP, position, imaginary, col);
     	}
 		
 		
 	}
+	
 }
